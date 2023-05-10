cac5cac4952b9827f6df0a7149c6844b097f908a
Talles de Almeida Oliveira <121827656+tallesalmeida@users.noreply.github.com>
2023-05-10 11:42:49 -0300

    att

---
+++ index.js
@@ -0,0 +1,8 @@
+/*document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        let fired_button = button.value;
+        alert(fired_button);
+        
+    });
+});*/
+
