9ce5498f99a9476277909e1988ebc4988161aaf1
Talles de Almeida Oliveira <121827656+tallesalmeida@users.noreply.github.com>
2023-05-10 14:13:00 -0300

    att

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+let aval = 0 
+let n = 0 
 
 document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', () => {
-        let aval = button.value;
-        alert(aval);
-    });
-});
+        aval = button.value
+    })
+})
 
 function submit() {
-    container.style.display = "none"
-    container2.style.display = "flex"
+    n = aval
+
+    if (n == 0) {
+        window.location.reload() //se não clicar em nenhum número a página só atualiza.
+    } else {
+        ratingnumber.innerHTML = (n)
+        container.style.display = "none"
+        container2.style.display = "flex"
+    }
+    
 }
 
