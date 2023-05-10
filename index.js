33835741f318eefebb9a11b7cf1e092344ad2a12
Talles de Almeida Oliveira <121827656+tallesalmeida@users.noreply.github.com>
2023-05-10 14:29:01 -0300

    att

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ function submit() {
     n = aval
 
     if (n == 0) {
-        window.location.reload() //se não clicar em nenhum número a página só atualiza.
+        window.location.reload() //se não clicar em nenhum número a página só atualiza. Poderia ter colocado um window.alert pedindo pra clicar em algum número. 
     } else {
         ratingnumber.innerHTML = (n)
         container.style.display = "none"
-        container2.style.display = "flex"
+        container2.style.display = "block"
     }
-    
+}
+
+function voltar() {
+    container.style.display = "block"
+    container2.style.display = "none"
 }
 
